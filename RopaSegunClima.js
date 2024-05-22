cadb310310317e6bbe457c6ea615dcd679306d0c
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-22 17:36:55 -0500

    nuevo

--- RopaSegunClima.js
+++ RopaSegunClima.js
@@ -9,19 +9,22 @@
 //Utiliza el siguiente código base y probá modificando el valor de temperatura 
 //para ver como reacciona tu código a distintos valores.
 
-// dato recibido desde la linea de comandos
 const argumento = process.argv[2];
-// convertimos el dato a número (los argumentos son textos por defecto)
 const temperatura = parseInt(argumento);
 
-let recomendacion; // variable donde se almacenará la recomendación de ropa
+let recomendacion; 
 
 if (temperatura < 5) {
-    console.log(temperatura)// tu código
+  recomendacion = "abrigo heavy";
 } else if (temperatura >= 5 && temperatura <= 15) {
-    console.log()// tu código
+  recomendacion = "un bucito y campera";
+} else if (temperatura >= 16 && temperatura <= 20) {
+  recomendacion = "sweater y pantalon largo";
+} else if (temperatura >= 21 && temperatura <= 25) {
+  recomendacion = "remera mangas cortas";
+} else {
+  recomendacion = "remera y shorts";
 }
-// otros else if
 
 console.log("La temperatura actual es de", temperatura)
 console.log("Te recomendamos usar", recomendacion);
