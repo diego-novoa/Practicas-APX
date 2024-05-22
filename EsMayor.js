ee14c6319ef31b4e4863f2df40315c433e6aba89
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-22 16:44:15 -0500

    actualizar

---
+++ EsMayor.js
@@ -0,0 +1,12 @@
+// vamos a usar persona.edad en el if para saber si es mayor
+
+const persona = { 
+    edad: 20
+  }
+  
+  if (persona.edad >= 18){
+    console.log("Es mayor")
+  } else {
+    console.log("No es mayor")
+  }
+  
