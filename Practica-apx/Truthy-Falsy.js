9f58d6830e0f3418532d60f5be3a63dc7399abf8
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-22 18:19:53 -0500

    revisado ok

---
+++ Practica-apx/Truthy-Falsy.js
@@ -0,0 +1,118 @@
+const numeroA = 10;
+const numeroB = 0;
+const numeroC = -5;
+
+const textoA = "Hola";
+const textoB = "";
+const textoC = "Mundo";
+
+const arrayA = [1, 2, 3];
+const arrayB = [];
+
+const objeto = { nombre: "Juan", edad: 30 };
+
+const booleanoA = true;
+const booleanoB = false;
+
+// # numeroA
+
+console.log("numeroA va a ser truthy");
+
+if (numeroA) {
+  console.log("El dato numeroA es truthy");
+} else {
+  console.log("El dato numeroA es falsy");
+}
+
+// ####################################################
+// Escribí tus predicciones antes de ejecutar el código
+
+// # numeroB
+console.log("falsy");
+console.log("numeroB va a ser falsy");
+
+if (numeroB) {
+  console.log("El dato numeroB es truthy");
+} else {
+  console.log("El dato numeroB es falsy");
+}
+
+// # textoA
+console.log("truthy");
+console.log("textoA va a ser truthy");
+
+if (textoA) {
+  console.log("El dato textoA es truthy");
+} else {
+  console.log("El dato textoA es falsy");
+}
+
+// # textoB
+console.log("falsy");
+console.log("textoB va a ser falsy");
+
+if (textoB) {
+  console.log("El dato textoB es truthy");
+} else {
+  console.log("El dato textoB es falsy");
+}
+
+// # textoC
+console.log("falsy");
+console.log("textoC va a ser falsy");
+
+if (textoC) {
+  console.log("El dato textoC es truthy");
+} else {
+  console.log("El dato textoC es falsy");
+}
+
+// # arrayA
+console.log("truthy");
+console.log("arrayA va a ser truthy");
+
+if (arrayA) {
+  console.log("El dato arrayA es truthy");
+} else {
+  console.log("El dato arrayA es falsy");
+}
+
+// # arrayB
+console.log("truthy");
+console.log("arrayB va a ser truthy");
+
+if (arrayB) {
+  console.log("El dato arrayB es truthy");
+} else {
+  console.log("El dato arrayB es falsy");
+}
+
+// # objeto
+console.log("truthy");
+console.log("objeto va a ser truthy");
+
+if (objeto) {
+  console.log("El dato objeto es truthy");
+} else {
+  console.log("El dato objeto es falsy");
+}
+
+// # booleanoA
+console.log("truthy");
+console.log("booleanoA va a ser truthy");
+
+if (booleanoA) {
+  console.log("El dato booleanoA es truthy");
+} else {
+  console.log("El dato booleanoA es falsy");
+}
+
+// # booleanoB
+console.log("falsy");
+console.log("booleanoB va a ser falsy");
+
+if (booleanoB) {
+  console.log("El dato booleanoB es truthy");
+} else {
+  console.log("El dato booleanoB es falsy");
+}
