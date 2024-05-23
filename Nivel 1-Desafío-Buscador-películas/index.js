c2423f089864a2cda97ee6baacf5d88685ae6fc8
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-23 17:58:19 -0500

    actualizado

---
+++ Nivel 1-Desafío-Buscador-películas/index.js
@@ -0,0 +1,13 @@
+[{
+  "title": "Titulo de la película",
+  "rating": 4,
+  "tags": ["acción", "favorita", "nueva"],
+  //otras propiedades
+},
+{
+  "title": "Titulo de la segunda película",
+  "rating": 3,
+  "tags": ["favorita", "nueva"],
+  //otras propiedades
+}]
+
