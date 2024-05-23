7ea69641d28d029c7ca03c750acdc945ade9ad11
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-22 19:08:01 -0500

    revisado

---
+++ Practica-apx/Switch.js
@@ -0,0 +1,22 @@
+// day es un valor del 1 al 7
+const day = 3;
+let dayName = "";
+
+switch (day) {
+    case 1:
+        dayName ="lunes"
+      break;
+    case 2:
+        dayName ="martes"
+      break;
+    default:
+  }
+  
+  
+  console.log("Hoy es", dayName);
+  
+  // acá agregá tu coídgo para asignarle a dayName el valor correcto
+  // por ejemplo si day es 2 dayName debería valer "Martes"
+  // switch {}
+
+  
