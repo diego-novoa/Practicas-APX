3f81e07abbf47a85f451a9e35f060ad0f2e919ae
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-23 19:46:31 -0500

    actualizar

---
+++ Condicionales/if-Condicional/es-par.js
@@ -0,0 +1,29 @@
+// Pedir dos números y decir si non múltiplos o no
+
+let numero1 = 12
+let numero2 = 24
+
+if(numero1 % numero2 === 0 || numero2 % numero1 ===0){
+	console.log("si es multiplo")
+}else{
+	console.log("no es multiplo")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// let numero1 = 36
+// let numero2 = 24
+//if(numero1 % numero2 === 0 || numero2 % numero1 === 0){
+//  console.log("son multiplos")
+//  }else{
+ // console.log("no son multiplos")
+//  }
