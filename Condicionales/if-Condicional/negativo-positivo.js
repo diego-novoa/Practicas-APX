d6cc032418e5acfb5b16122e2840962c77cfefd2
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-23 21:42:34 -0500

    actualizar

---
+++ Condicionales/if-Condicional/negativo-positivo.js
@@ -0,0 +1,10 @@
+//Pedir un número y decir si es un número negativo, si es positivo o cero
+let numero =0
+
+if(numero > 0){
+  console.log("es un numero positivo ");
+}else if(numero < 0){
+  console.log("es un numero negativo");
+}else{
+	console.log("es el numero cero (0)")
+}
