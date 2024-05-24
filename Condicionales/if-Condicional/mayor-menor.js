fb1e1c0d8e88477499330753f30ec1a8d897f8c1
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-23 20:08:45 -0500

    actual

---
+++ Condicionales/if-Condicional/mayor-menor.js
@@ -0,0 +1,32 @@
+// Pedir dos números y decir cual es el mayor
+
+let numero1 = 23
+let numero2 = 44
+
+if(numero1 > numero2){
+	console.log("el numero mayor es:",numero1)
+}else{
+	console.log("el numero mayor es:",numero2)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let numero1 = 54
+//let numero2 = 35
+
+//if(numero1 > numero2){
+//	console.log("el numero mayor es:",numero1)
+//}else{
+//	console.log("el numero menor es:",numero2)
+//}
