6fd6596892b593342093815517cfeeb3d827f9db
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-25 22:18:04 -0500

    ok

--- Condicionales/pedir-nota.js
+++ Condicionales/pedir-nota.js
@@ -1,15 +1,17 @@
 //Pedir una calificación  de 0 a 10 y mostrar de la siguiente manera: 
 // Insuficiente, Suficiente, Bien, Excelente
 
-let nota = 11
+let calificación = 11
 
-if (nota <= 11){
-  console.log("su nota es Excelente")
-}else if(nota <= 7){
-	console.log("su nota es Bien")
-}else if (nota <= 6 ){
-	console.log("Suficiente")
-}else if (nota <= 5){
-	console.log("Insuficiente")
+if (calificación <5 && calificación >=0){
+  console.log("calificación: Insuficiente ")
+}else if(calificación === 5){
+	console.log("calificación: Suficiente")
+}else if (calificación >5 && calificación <=8 ){
+	console.log("calificación: Bien")
+}else if (calificación >8 && calificación <=10){
+	console.log("calificación: Excelente")
+}else{
+	console.log("calificación no valida")
 }
     
