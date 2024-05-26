a1e55a7845d6709016a97cc2f81521e883cd8842
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-25 21:57:00 -0500

    nuevo

--- Condicionales/negativo-positivo.js
+++ Condicionales/negativo-positivo.js
@@ -8,3 +8,21 @@ if(numero > 0){
 }else{
 	console.log("es el numero cero (0)")
 }
+
+
+
+
+
+
+
+
+
+//let numero =0
+
+//if(numero > 0){
+  //console.log("es un numero positivo ");
+//}else if(numero < 0){
+  //console.log("es un numero negativo");
+//}else{
+	//console.log("es el numero cero (0)")
+//}
---
+++ Condicionales/pedir-nota.js
@@ -0,0 +1,3 @@
+//Pedir una calificación  de 0 a 10 y mostrar de la siguiente manera: Insuficiente, Suficiente, Bien, Excelente
+
+let nota =11
