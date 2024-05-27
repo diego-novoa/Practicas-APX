b31ad2b78afb6c07ba808a4950fb705b01612138
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-26 19:08:14 -0500

    actualizar

---
+++ Condicionales/redondear.js
@@ -0,0 +1,9 @@
+//Juan tiene N dólares, David tiene la mitad de lo que posee Juan y José 
+//la mitad de lo que poseen Juan y David juntos. 
+//Mostrar el promedio de la cantidad de dinero que tienen entre los tres y redondearlo
+
+let juan=20;
+let david=juan /2;
+let jose= (juan + david)/2;
+
+  
