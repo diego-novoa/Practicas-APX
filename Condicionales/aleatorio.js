3c8db8c268cc2dd5811b0ac1cd454bb9170669f4
“DIEGO <diegonovoa1981@hotmail.com>
2024-05-26 21:17:52 -0500

    vale

--- Condicionales/aleatorio.js
+++ Condicionales/aleatorio.js
@@ -1,3 +1,5 @@
 //Crear un número aleatorio entre el 1 y 20, si es par mostrar true seguido 
 //el número si es impar mostrar  false seguido del número, 
 //utilizar el operador ternario.
+
+let numero1 = 2;
--- Condicionales/mostrar-minuscu.js
+++ Condicionales/mostrar-minuscu.js
@@ -2,3 +2,20 @@
 //mostrar false si todas son minúsculas mostrar true, 
 //utilizar el operador ternario.
 
+let texto = "hola mundo"
+let comparacion = texto;
+const respuesta = ( comparacion.toLowerCase() === texto ) ? true : false;
+
+console.log(respuesta)
+
+
+
+
+
+
+//let texto = "hola mundo"
+//et comparacion = texto;
+//const respuesta = ( comparacion.toLowerCase() === texto ) ? true : false;
+
+//console.log(respuesta)
+
